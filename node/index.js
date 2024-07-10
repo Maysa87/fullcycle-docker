29ff6ff02e7d17fa42d7ce8438b138f071181061
Maysa Leocadio <lmaysa.gomes@gmail.com>
2024-07-10 14:12:29 -0300

    nginx as proxy

--- node/index.js
+++ node/index.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const connection = require('./database');
 
 app.get('/', (req, res) => {
-    res.send('Full Cycle Rocks!');
+    const name = 'User ' + Math.floor(Math.random() * 1000);
+    connection.query(`INSERT INTO people (name) VALUES ('${name}')`, (err) => {
+        if (err) {
+            console.error('Error inserting into database:', err);
+            return res.status(500).send('Internal Server Error');
+        }
+        connection.query('SELECT name FROM people', (err, results) => {
+            if (err) {
+                console.error('Error querying database:', err);
+                return res.status(500).send('Internal Server Error');
+            }
+            let response = '<h1>Full Cycle Rocks!</h1><ul>';
+            results.forEach(row => {
+                response += `<li>${row.name}</li>`;
+            });
+            response += '</ul>';
+            res.send(response);
+        });
+    });
 });
 
-app.listen(port, () => {
-    console.log(`listening at :${port}`);
+app.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
